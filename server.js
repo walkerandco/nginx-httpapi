fd9e57511e77228ddb790cde345bf0d6b76221a6
Steven Walker-Roberts <steven@walkerroberts.co.uk>
2018-02-03 10:33:26 +0000

    further initial prep

---
+++ server.js
@@ -0,0 +1,17 @@
+"use strict";
+
+module.exports = {
+  /**
+   * [description]
+   * @param  {[type]} a [description]
+   * @param  {[type]} b [description]
+   * @return {[type]}   [description]
+   */
+  testsum: function (a, b) {
+    return a + b;
+  }
+  // test > testsum function
+  // # testsum(1, 2) == 3 (returns the sum of both params)
+  // # testsum(3, 4) == 7 (returns the sum of both params)
+
+}
