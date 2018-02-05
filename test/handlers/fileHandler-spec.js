89203588e620d6eb5c080fd0bb0d82ad524c3ab3
Steven Walker-Roberts <steven@walkerroberts.co.uk>
2018-02-05 16:55:31 +0000

    Added initial configuration writer.

--- test/handlers/fileHandler-spec.js
+++ test/handlers/fileHandler-spec.js
@@ -1,13 +1,27 @@
 var assert = require('assert');
 var test = require('jasmine');
-var file = require('/home/swr/git/nginx-httpapi/lib/handlers/fileHandler.min.js');
+var file = require('/home/swr/git/nginx-httpapi/lib/handlers/fileHandler.js');
+
 
 describe('fileHandler readConfig function', () => {
-  const out = file.readConfig('/home/swr/git/nginx-httpapi/test/res/nginx.conf');
+  let out = file.readConfig('/home/swr/git/nginx-httpapi/test/res/nginx.conf');
   it('should read nginx conf file', () => {
     expect(out).toBeTruthy();
   });
   it('should read in as containing more than 1 character', () => {
     expect(out.toString().length).toBeGreaterThan(1);
+  });
+});
+
+describe('fileHandler writeConfig function', () => {
+  beforeEach(() => {
+    file.writeConfig('/home/swr/git/nginx-httpapi/test/res/nginx.conf.test', "test");
+  });
+  it('should read in a value of "test"', () => {
+    try {
+      file.readConfig('/home/swr/git/nginx-httpapi/test/res/nginx.conf.test', (out) => {expect(out).toContain("test")});
+    } catch (e) {
+      console.error(e);
+    }
   });
 });
