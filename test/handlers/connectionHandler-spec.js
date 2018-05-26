039a3281efdd237a05a93b9c393770df0cdafdd4
Steven Walker-Roberts <steven@walkerroberts.co.uk>
2018-05-26 15:15:37 +0100

    general improvements

---
+++ test/handlers/connectionHandler-spec.js
@@ -0,0 +1,15 @@
+let path = require('path');
+let assert = require('assert');
+let expect = require('chai').expect;
+let sinon = require('sinon');
+let file = require('../../lib/handlers/fileHandler.js');
+
+describe('connectionHandler class', () => {
+  describe('init', () => {
+    it('should return a http server instance', async () => {
+
+    });
+
+  });
+
+});
